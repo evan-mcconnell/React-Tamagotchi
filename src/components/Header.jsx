b0e7a4110dc1dfb2126686bdafe28f69cd0a050e
Joe Pritchett and Kimberly McConnell <kimberly@>
2019-04-24 16:43:44 -0700

    add sleeping function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header(){
+  var linkStyle = {
+    fontColor: "#111"
+  }
   return (
     <div>
       <h1>Tamagotchi</h1>
-      <Link to="/">Your Pet</Link> | <Link to="/store">Store</Link> | <Link to="/work">Work</Link>
+      <Link style={linkStyle} to="/">Your Pet</Link> | <Link to="/store">Store</Link> | <Link to="/work">Work</Link>
     </div>
   );
 }
